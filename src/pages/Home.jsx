51fc6e12477f99bfcaffaff4df66c8ef298add49
Mohammad Nikzad <nikzad.engineer@gmail.com>
2022-03-15 21:27:36 +0330

    Clean code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import UserResults from "../components/users/UserResults"
-import UserSearch from "../components/users/UserSearch"
+import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
 
 function Home() {
-    return (
-        <>
-            <UserSearch />
-            <UserResults />
-        </>
-    )
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  )
 }
 
 export default Home
